b9b12400d680fab40a1fb8853a6ef9b47225c446
XmL <mac@bogon.(none)>
2013-03-15 09:12:36 +0800

    Initial Commit

---
+++ MyPhoneCall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyPhoneCall
+//
+//  Created by XmL on 13-3-15.
+//  Copyright (c) 2013年 XmL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyPhoneCall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyPhoneCall
+//
+//  Created by XmL on 13-3-15.
+//  Copyright (c) 2013年 XmL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
